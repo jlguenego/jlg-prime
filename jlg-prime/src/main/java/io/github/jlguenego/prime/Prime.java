1f1ef9acf0550db8c571587bb46de76a19c2db3d
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-07-06 17:47:53 +0200

    dev: added javadoc

--- jlg-prime/src/main/java/io/github/jlguenego/prime/Prime.java
+++ jlg-prime/src/main/java/io/github/jlguenego/prime/Prime.java
@@ -3,7 +3,16 @@
  */
 package io.github.jlguenego.prime;
 
+/**
+ * Class that offers tools about Prime numbers
+ */
 public class Prime {
+
+    /**
+     * Test if a number is prime (can be divided only by 1 and by itself).
+     * @param nbr - the number to test
+     * @return true if prime, false otherwise.
+     */
     public static boolean isPrime(int nbr) {
         double sqrt = Math.sqrt(nbr);
         for (int i = 2; i < sqrt; i++) {
